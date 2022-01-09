76d660c1fa376a036be3936f9d78b879f8089a87
Krzysztof Piekarczyk <krzys@piekarczyk.eu>
2022-01-09 21:49:28 +0100

    channel extraction

--- face_mesh_exploration.py
+++ face_mesh_exploration.py
@@ -13,27 +13,21 @@ def set_res(cap, x, y):
 
 
 def main():
-    mp_drawing = mp.solutions.drawing_utils
-    mp_drawing_styles = mp.solutions.drawing_styles
     mp_face_mesh = mp.solutions.face_mesh
-
-    min_YCrCb = np.array([0, 133, 77], np.uint8)
-    max_YCrCb = np.array([235, 173, 127], np.uint8)
-
-    # For webcam input:
-    drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
     cap = cv2.VideoCapture(0)
-
     set_res(cap, 1280, 720)
 
     start_time = datetime.now()
+    face_found = False
+    frame_count = 0
 
     with mp_face_mesh.FaceMesh(
             max_num_faces=1,
-            refine_landmarks=False,
+            refine_landmarks=True,
             min_detection_confidence=0.5,
             min_tracking_confidence=0.5) as face_mesh:
         while cap.isOpened():
+            frame_count += 1
             success, image = cap.read()
             face_mask = np.zeros(image.shape[:2], np.uint8)
             if not success:
@@ -50,6 +44,7 @@ def main():
             image.flags.writeable = True
             image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
             if results.multi_face_landmarks:
+                face_found = True
                 h, w, c = image.shape
 
                 face_landmarks = results.multi_face_landmarks[0]
@@ -68,22 +63,41 @@ def main():
 
                 face_image = masked_image[min_y:max_y, min_x:max_x]
             else:
+                face_found = False
                 face_image = np.zeros((200, 200), np.uint8)
 
             image = cv2.flip(image, 1)
             face_image = cv2.flip(face_image, 1)
 
             # Naively calculate framerate
+
+            info_bar = np.zeros((100, 1280), np.uint8)
+
             current_time = datetime.now()
             dt = (current_time - start_time).microseconds / 1e6
             start_time = current_time
             if dt > 0:
                 fps = int(1.0 / dt)
                 cv2.putText(
-                    image, f"FPS: {fps}", (10, 15), cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255))
+                    info_bar, f"FPS: {fps}", (10, 15), cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255))
+            
+            try:
+                cv2.imshow('Original image', image)
 
-            cv2.imshow('Original image', image)
-            cv2.imshow('Face', face_image)
+                if face_found:
+                    cv2.imshow('Face', face_image)
+
+                    B = face_image[:,:,0].sum()
+                    G = face_image[:,:,1].sum()
+                    R = face_image[:,:,2].sum()
+
+                    cv2.putText(info_bar, f"R: {R}", (300, 15), cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255))
+                    cv2.putText(info_bar, f"G: {G}", (300, 30), cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255))
+                    cv2.putText(info_bar, f"B: {B}", (300, 45), cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255))
+
+                cv2.imshow('Information', info_bar)
+            except:
+                print("Something is going wrong!")
 
             if cv2.waitKey(5) & 0xFF == 27:
                 break
