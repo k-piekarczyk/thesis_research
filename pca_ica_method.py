639d2c762ef47545dd0cbb571b13ec6e87cc1c9f
Krzysztof Piekarczyk <krzys@piekarczyk.eu>
2022-01-11 00:48:47 +0100

    sorta working

--- pca_ica_method.py
+++ pca_ica_method.py
@@ -1,8 +1,12 @@
 import cv2
 import sys
 import mediapipe as mp
-from datetime import datetime
+from datetime import datetime, time, timedelta
 import numpy as np
+import traces
+
+from scipy import signal
+from sklearn.decomposition import FastICA, PCA
 
 from pyqtgraph.Qt import QtGui, QtCore
 import numpy as np
@@ -11,6 +15,17 @@ import pyqtgraph as pg
 from utils.face_mask import get_face_contour
 from utils.fixed_list import FixedList
 
+FRAME_RATE = 30
+FRAME_PERIOD_MS = 1000.0 / 30
+
+TIME_SERIES_SAMPLE_LENGTH = 256
+TIME_SERIES_TD = timedelta(
+    milliseconds=FRAME_PERIOD_MS * TIME_SERIES_SAMPLE_LENGTH)
+
+F1 = 0.5
+F2 = 3.7
+
+FIR = signal.firwin(32, [F1, F2], window="hamming", pass_zero=False, fs=30)
 
 def set_res(cap, x, y):
     cap.set(3, int(x))
@@ -21,40 +36,91 @@ def set_res(cap, x, y):
 def main():
     mp_face_mesh = mp.solutions.face_mesh
     cap = cv2.VideoCapture(0)
-    set_res(cap, 1280, 720)
+    # set_res(cap, 1280, 720)
 
     start_time = datetime.now()
     face_found = False
     frame_count = 0
-    
-    value_list = FixedList(256)
+
+    value_list = FixedList(TIME_SERIES_SAMPLE_LENGTH)
 
     # Setting up graphing
     app = pg.mkQApp("Plotting Example")
 
     win = pg.GraphicsLayoutWidget(show=True, title="Basic plotting examples")
-    win.resize(1000,600)
-    win.setWindowTitle('pyqtgraph example: Plotting')
+    win.resize(1500, 600)
+    win.setWindowTitle('Raw color channels')
 
     pg.setConfigOptions(antialias=True)
 
-    plot = win.addPlot(title="Updating plot")
+    p_r = win.addPlot(title="Red channel")
+    p_r_ICA = win.addPlot(title="Red channel: ICA")
+    win.nextRow()
+    p_g = win.addPlot(title="Green channel")
+    p_g_ICA = win.addPlot(title="Green channel: ICA")
+    win.nextRow()
+    p_b = win.addPlot(title="Blue channel")
+    p_b_ICA = win.addPlot(title="Blue channel: ICA")
 
-    r_curve = plot.plot(pen="r")
-    g_curve = plot.plot(pen="g")
-    b_curve = plot.plot(pen="b")
+    r_curve = p_r.plot(pen="r")
+    g_curve = p_g.plot(pen="g")
+    b_curve = p_b.plot(pen="b")
+
+    r_curve_ICA = p_r_ICA.plot(pen="r")
+    g_curve_ICA = p_g_ICA.plot(pen="g")
+    b_curve_ICA = p_b_ICA.plot(pen="b")
 
     def update():
         l = value_list.get()
-        t = [x[3] for x in l]
-        R = [x[2] for x in l]
-        G = [x[1] for x in l]
-        B = [x[0] for x in l]
 
-        r_curve.setData(R)
-        g_curve.setData(G)
-        b_curve.setData(B)
-    
+        if len(l) > int(TIME_SERIES_SAMPLE_LENGTH/5) + 1:
+            t = [x[3] for x in l]
+            R = [x[0] for x in l]
+            G = [x[1] for x in l]
+            B = [x[2] for x in l]
+
+            R_z = list(zip(t, R))
+            G_z = list(zip(t, G))
+            B_z = list(zip(t, B))
+
+            R_ts = traces.TimeSeries(R_z)
+            G_ts = traces.TimeSeries(G_z)
+            B_ts = traces.TimeSeries(B_z)
+
+            start_t = t[0]
+            end_t = t[-1]
+
+            R_resampled_ts = R_ts.sample(sampling_period=timedelta(milliseconds=FRAME_PERIOD_MS), start=start_t, end=end_t, interpolate="linear")
+            G_resampled_ts = G_ts.sample(sampling_period=timedelta(milliseconds=FRAME_PERIOD_MS), start=start_t, end=end_t, interpolate="linear")
+            B_resampled_ts = B_ts.sample(sampling_period=timedelta(milliseconds=FRAME_PERIOD_MS), start=start_t, end=end_t, interpolate="linear")
+
+            R_resampled = [x[1] for x in R_resampled_ts]
+            G_resampled = [x[1] for x in G_resampled_ts]
+            B_resampled = [x[1] for x in B_resampled_ts]
+
+            filtered_R = signal.lfilter(FIR, 1.0, R_resampled)[int(TIME_SERIES_SAMPLE_LENGTH/5):]
+            filtered_G = signal.lfilter(FIR, 1.0, G_resampled)[int(TIME_SERIES_SAMPLE_LENGTH/5):]
+            filtered_B = signal.lfilter(FIR, 1.0, B_resampled)[int(TIME_SERIES_SAMPLE_LENGTH/5):]
+
+            r_curve.setData(R_resampled)
+            g_curve.setData(G_resampled)
+            b_curve.setData(B_resampled)
+
+            pca = PCA()
+            data = np.vstack((filtered_R, filtered_G, filtered_B)).reshape(-1, 3)
+            comp = pca.fit_transform(data)
+
+            # print(comp.shape)
+
+            comp_1 = comp[:, 0]
+            comp_2 = comp[:, 1]
+            comp_3 = comp[:, 2]
+
+            r_curve_ICA.setData(comp_1)
+            g_curve_ICA.setData(comp_2[comp_2 < 100000])
+            b_curve_ICA.setData(comp_3)
+            
+
     timer = QtCore.QTimer()
     timer.timeout.connect(update)
     timer.start(50)
@@ -92,8 +158,9 @@ def main():
                 cv2.drawContours(
                     face_mask, [face_contour_points], 0, (255, 255, 255), -1)
 
-                min_x, min_y, br_w, br_h = cv2.boundingRect(face_contour_points)
-                
+                min_x, min_y, br_w, br_h = cv2.boundingRect(
+                    face_contour_points)
+
                 max_x = min_x + br_w
                 max_y = min_y + br_h
 
@@ -118,23 +185,32 @@ def main():
                 fps = int(1.0 / dt)
                 cv2.putText(
                     info_bar, f"FPS: {fps}", (10, 15), cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255))
-            
+            cv2.putText(
+                    info_bar, f"Samples: {len(value_list.get())}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255))
+
             try:
                 cv2.imshow('Original image', image)
 
                 if face_found:
                     cv2.imshow('Face', face_image)
 
-                    current_R = face_image[:,:,2].sum()
-                    current_G = face_image[:,:,1].sum()
-                    current_B = face_image[:,:,0].sum()
+                    in_range = cv2.inRange(face_image, np.array([1, 1, 1]), np.array([255, 255, 255]))
+                    nb_count = cv2.countNonZero(in_range)
+
+                    current_R = face_image[:, :, 2].sum() / nb_count
+                    current_G = face_image[:, :, 1].sum() / nb_count
+                    current_B = face_image[:, :, 0].sum() / nb_count
                     current_t = datetime.now()
 
-                    cv2.putText(info_bar, f"R: {current_R}", (300, 15), cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255))
-                    cv2.putText(info_bar, f"G: {current_G}", (300, 30), cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255))
-                    cv2.putText(info_bar, f"B: {current_B}", (300, 45), cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255))
+                    cv2.putText(
+                        info_bar, f"R: {current_R}", (300, 15), cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255))
+                    cv2.putText(
+                        info_bar, f"G: {current_G}", (300, 30), cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255))
+                    cv2.putText(
+                        info_bar, f"B: {current_B}", (300, 45), cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255))
 
-                    value_list.push([current_R, current_G, current_B, current_t])
+                    value_list.push(
+                        [current_R, current_G, current_B, current_t])
 
                 cv2.imshow('Information', info_bar)
             except:
